afccb7d9119aaf565e1fe63477bfecb000fa7f6e
Sheina Cunningham <cunninghamsheina@gmail.com>
2021-03-11 18:40:35 -0500

    Css styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,10 @@
        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
        let str = document.getElementById("search").value.trim();
        url = url.concat(str);
-       console.log(url);
        fetch(url)
          .then(response => response.json())
          .then(content => {
-
            //  data, pagination, meta
-           console.log(content.data);
-           console.log("META", content.meta);
            let fig = document.createElement("figure");
            let img = document.createElement("img");
            let fc = document.createElement("figcaption");
